f47a245f0b4e765a4628a3edf6ef9ddfcdc8caba
mooss <mooss@protonmail.com>
2025-08-28 14:54:39 +0200

    Remove dependency on stow

--- setup-links.bash
+++ setup-links.bash
@@ -7,17 +7,50 @@ cd "$HERE"
 # This script links all files (not directories) from ./config into ~/.config using stow.
 # It creates any missing directories in ~/.config as needed.
 
-# Creates all the subdirs of $source below $dest.
-function makedirs() {
-    local -r source="$1"; local -r dest="$2"
-    find "$source" -type d -printf "$HOME/$dest/%P\n" | xargs mkdir -pv
+# Creates a link if it does not already exists.
+function link-file() {
+    local -r from="$1"; local -r to="$2"
+    if ! test -e "$to"; then
+        ln -vs "$from" "$to"
+    fi
+}
+
+# Creates the missing links and directories for one dotfile.
+function create-one() {
+    local -r sourcedir="$1"; local -r dest="$2"; local -r path="$3"
+    local -r sourcepath="$PWD/$sourcedir/$path"; local -r destpath="$HOME/$dest/$path"
+
+    if test -d "$sourcepath"; then
+        mkdir -pv "$destpath"
+    elif test -f "$sourcepath"; then
+        link-file "$sourcepath" "$destpath"
+    else
+        echo >&2 "$sourcepath is not a file or directory"
+    fi
+}
+
+# Creates the corresponding link or directory for all entries of null-separated STDIN.
+function create-all() {
+    local -r source="$1"; local -r dest="$2"; local path
+
+    while IFS= read -r -d '' path; do
+        create-one "$source" "$dest" "$path"
+    done
 }
 
 # Links all the files contained below $source to $dest.
 function lnk() {
     local -r source="$1"; local -r dest="$2"
-    makedirs "$source" "$dest"
-    stow -v "$source" -t "$HOME/$dest"
+
+    if test -d "$source"; then
+        find "$source" -printf "%P\n" | grep . | tr '\n' '\0' | create-all "$source" "$dest"
+    else
+        link-file "$PWD/$source" "$HOME/$dest"
+    fi
 }
 
-lnk config .config
+DOTTED='config'
+for file in $DOTTED; do
+    lnk $file .$file
+done
+
