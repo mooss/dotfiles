d5f5f1025e512d5a2a000a6d77897e4823e78303
mooss <mooss@protonmail.com>
2025-09-03 17:00:56 +0200

    Fix zsh word forward and backward behavior

--- config/zsh/config.zsh
+++ config/zsh/config.zsh
@@ -1,2 +1,6 @@
+# Prevent the insufferable default behavior of skipping the space whith word forward and backwards.
+bindkey '^[f' emacs-forward-word
+bindkey '^[b' emacs-backward-word
+
 setopt nocorrectall
 PATH=$PATH:$HOME/.local/bin
